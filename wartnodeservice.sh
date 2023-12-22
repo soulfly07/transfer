e885030a699313c44bd1e3ab9ba53b13ec8b8e7c
soulfly07 <55403602+soulfly07@users.noreply.github.com>
2023-12-23 00:30:56 +0200

    Add files via upload

---
+++ wartnodeservice.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+#public settings
+currentPath=/home/user
+path=$currentPath/wartnode
+serviceScript=wartnode.sh
+servicePath=/etc/systemd/system
+nameService=wartnode.service
+
+#stop service if it is running
+systemctl is-active --quiet wartnode && systemctl stop --no-block wartnode
+
+
+cd /home/user
+mkdir .warthog
+
+FILE=wartnode
+if [ ! -d "$FILE" ]; then
+    mkdir wartnode
+fi
+cd wartnode
+
+wget -O wart-node-linux https://github.com/warthog-network/Warthog/releases/download/master-0.1.2-6d298a3/wart-node-linux
+echo -e "#!/usr/bin/bash\n/home/user/wartnode/wart-node-linux --rpc=0.0.0.0:3000 --chain-db=/home/user/.warthog/chain.db3 --peers-db=/home/user/.warthog/peers.db3 >/home/user/wartnode/wartnode.log" >/home/user/wartnode/wartnode.sh
+chmod +x *
+
+
+echo -e "[Unit]\nAfter=network-online.target\n[Service]\n#StandardOutput=append:wartnode/wartnode.log\n#StandardError=append:wartnode/wartnode.error.log\nExecStart=/bin/bash $path/$serviceScript\nRestart=on-failure\nRestartSec=1s\n[Install]\nWantedBy=default.target" > $servicePath/$nameService
+
+
+chmod 664 $servicePath/$nameService
+systemctl daemon-reload
+systemctl enable --no-block $nameService
+systemctl start --no-block $nameService
+
+chmod u+x $path/$serviceScript
